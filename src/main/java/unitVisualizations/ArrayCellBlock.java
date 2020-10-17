7048f616febd374116215a5d7a005b109cd96015
Varun Venkatesh <vaarunven@gmail.com>
2020-10-17 12:25:19 -0700

    needsresize implemented

--- src/main/java/unitVisualizations/ArrayCellBlock.java
+++ src/main/java/unitVisualizations/ArrayCellBlock.java
@@ -21,7 +21,7 @@ public class ArrayCellBlock <T> {
     private String cellColor;
     private T data;
 
-    public ArrayCellBlock(int capacity, T data){
+    public ArrayCellBlock(int capacity, int size, T data){
         this.capacity = capacity;
         width = CELL_WIDTH;
         height = CELL_HEIGHT;
@@ -30,15 +30,31 @@ public class ArrayCellBlock <T> {
         this.data = data;
     }
 
-    public <T> void addToBlock(T data){
-
+    public Resize needsResize(int capacity, int size){
+        if(size==capacity && capacity>10){
+            return Resize.LARGE;
+        }
+        if(size <= capacity/4 && capacity>10){
+            return Resize.SMALL;
+        }
+        return Resize.NO;
     }
 
-    public void updateSize(int newCapacity){
+    public <T> void addToBlock(T data){
+        if(needsResize())
+    }
+
+    public void updateCapacity(int newCapacity){
         capacity = newCapacity;
         //update width, height, textsize
         //should zoom be a physical zoom or just change width height based on how many times they moved their hands
 
+    }
+
+    private enum Resize{
+        LARGE,
+        SMALL,
+        NO
     }
 
 }
