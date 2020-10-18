8a51ec65d80efba3ebb229fd1001fc6104f9cbd2
Eric Marnadi <ericmarnadi@outlook.com>
2020-10-17 23:48:17 -0700

    Update InteractDebugger.cpp
    Reads lines how it's supposed to, writes each line to file, need to change file output path

--- InteractDebugger.cpp
+++ InteractDebugger.cpp
@@ -3,25 +3,31 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <winuser.h>
+#include <regex>
+#include <fstream>
+#include <string.h> 
 using namespace std;
 
-void F(int keyCode) {
-    keybd_event(keyCode, 0, 0, 0);
-    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
-}
+int GetLineNumber(string line);
+string ReadClipboardData();
+string GetLineFromFile(string filename, int n);
 struct KeyCommand
 {
     string command;
     int keyCode;
 };
-
-static struct KeyCommand keys[]= {
+static struct KeyCommand keys[] = {
     {"F5", 116},
     {"F7", 118},
     {"F8", 119},
     {"F9", 120},
     {"F10", 121}
 };
+static string outputFile = "code.txt";
+void F(int keyCode) {
+    keybd_event(keyCode, 0, 0, 0);
+    keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
+}
 
 int FN(string command) {
     for (int i = 0; i < 5; i++) {
@@ -38,11 +44,11 @@ void SHIFT(int f) {
 
 }
 void Copy() {
+    Sleep(1000);
     keybd_event(17, 0, 0, 0);
     keybd_event(67, 0, 0, 0);
     keybd_event(67, 0, KEYEVENTF_KEYUP, 0);
     keybd_event(17, 0, KEYEVENTF_KEYUP, 0);
-
 }
 void CTRL(int f, BOOL shift) {
     keybd_event(17, 0, 0, 0);
@@ -54,6 +60,33 @@ void CTRL(int f, BOOL shift) {
     }
     keybd_event(17, 0, KEYEVENTF_KEYUP, 0);
 }
+string ReadClipboardData()
+{
+    HANDLE h;
+    if (!OpenClipboard(NULL))
+        exit(EXIT_FAILURE);
+    h = GetClipboardData(CF_TEXT);
+    char* text = (char*)h;
+    string s = (string)text;
+    CloseClipboard();
+    return s;
+}
+int GetLineNumber(string line) {
+    int colon = line.find(':');
+    int comma = line.find(',');
+    int num = atoi(line.substr(colon + 1, comma - colon).c_str());
+    return num;
+}
+string GetLineFromFile(string filename, int n) {
+    string line;
+    ifstream f(filename);
+    for (int i = 0; i < n; i++)
+    {
+        getline(f, line);
+    }
+    return line;
+}
+
 // This gets called by winapi for every window on the desktop
 BOOL CALLBACK EnumWindowsProc(HWND windowHandle, LPARAM lParam) {
     DWORD searchedProcessId = (DWORD)lParam;  // This is the process ID we search for (passed from BringToForeground as lParam)
@@ -86,35 +119,28 @@ void CallCommand(string command) {
   } 
   Copy();
 }
-void BailOut()
-{
-    fprintf(stderr, "Exiting\n");
-    exit(EXIT_FAILURE);
-}
-
-void ReadClipboardData()
-{
-    HANDLE h;
-
-    if (!OpenClipboard(NULL))
-        BailOut();
-
-    h = GetClipboardData(CF_TEXT);
-
-    printf("%s\n", (char*)h);
-
-    CloseClipboard();
+void WriteToFile(string text) {
+    ofstream f;
+    f.open(outputFile);
+    f << text;
+    f.close();
 }
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
+    if (argc != 4) {
         cout << "Please pass in the correct number of arguments\n";
         return EXIT_FAILURE;
     }
     int pid = atoi(argv[1]);
     string command = argv[2];
+    string filename = argv[3];
     BringToForeground(pid);
     CallCommand(command);
-    ReadClipboardData();
+    string clip = ReadClipboardData();
+    int linenum = GetLineNumber(clip);
+    string line = GetLineFromFile(filename, linenum);
+    WriteToFile(line);
+    cout << "clip: " << clip << " linenum: " << linenum << endl;
+    cout << "Line From File: " << GetLineFromFile(filename, linenum) << endl;
 
 }   
